eb640529aa09efd414a9631d30ced8662758ca41
Chetan1330 <1330chetan@gmail.com>
2023-08-22 16:48:27 +0530

    Added Vercel Configs

--- newproject/settings.py
+++ newproject/settings.py
@@ -13,7 +13,8 @@ SECRET_KEY = 'mg_w_i-p&ploiq#um#h_7-ki0*n8+j6t^z6w4n5e9!+c31ly&$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','192.241.133.172',]
+# ALLOWED_HOSTS = ['127.0.0.1','192.241.133.172',]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
