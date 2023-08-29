7eefbd52da40899a1cf0cbe6c620631121fc195c
Chetan1330 <41874307+Chetan1330@users.noreply.github.com>
2023-08-29 13:41:51 +0530

    Update settings.py

--- newproject/settings.py
+++ newproject/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'mg_w_i-p&ploiq#um#h_7-ki0*n8+j6t^z6w4n5e9!+c31ly&$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','192.241.133.172','46.101.170.185','localhost']
+ALLOWED_HOSTS = ['34.71.250.187','127.0.0.1','192.241.133.172','46.101.170.185','localhost']
 
 
 # Application definition
